fa08ba3644c870b347a1fe70fdb4898e27ea26c2
AlessandroAloise <54986411+AlessandroAloise@users.noreply.github.com>
2020-11-19 15:29:50 +0100

    modifica del form
    aggiunta dell'aggiornamento del database

--- APIARIO/src/AddArnia.java
+++ APIARIO/src/AddArnia.java
@@ -1,13 +1,16 @@
+
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 /**
  * Schermata di dialogo per aggiungere un arina
- * 
+ *
  * @author alessandro.aloise
  * @version 29.10.2020
  */
 public class AddArnia extends javax.swing.JDialog {
+
+    public String[] valori = new String[3];
 
     public AddArnia(java.awt.Frame parent) {
         super(parent);
@@ -24,6 +27,16 @@ public class AddArnia extends javax.swing.JDialog {
         }
     }
 
+    public void setValori() {
+        valori[0] = testo.getText();
+        valori[1] = tLocalita.getText();
+        valori[2] = abitata.getText();
+       
+    }
+
+    public String getValori(int indice) {
+        return valori[indice];
+    }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -41,6 +54,7 @@ public class AddArnia extends javax.swing.JDialog {
         jScrollPane1 = new javax.swing.JScrollPane();
         tLocalita = new javax.swing.JTextPane();
         invio = new javax.swing.JButton();
+        abitata = new javax.swing.JCheckBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Nome Arina");
@@ -60,7 +74,7 @@ public class AddArnia extends javax.swing.JDialog {
         nazione.setText("Nazione:     ");
         naz.add(nazione, new java.awt.GridBagConstraints());
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antartide", "Antigua e Barbuda", "Arabia Saudita", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaigian", "Bahamas", "Bahrein", "Bangladesh", "Barbados", "Belgio", "Belize", "Benin", "Bermuda", "Bhutan", "Bielorussia", "Birmania", "Bolivia", "Bosnia ed Erzegovina", "Botswana", "Brasile", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambogia", "Camerun", "Canada", "Capo Verde", "Ciad", "Cile", "Cina", "Cipro", "Citt", "Colombia", "Comore", "Corea del Nord", "Corea del Sud", "Costa d'Avorio", "Costa Rica", "Croazia", "Cuba", "Cura", "Danimarca", "Dominica", "Ecuador", "Egitto", "El Salvador", "Emirati Arabi Uniti", "Eritrea", "Estonia", "Etiopia", "Figi", "Filippine", "Finlandia", "Francia", "Gabon", "Gambia", "Georgia", "Georgia del Sud e isole Sandwich meridionali", "Germania", "Ghana", "Giamaica", "Giappone", "Gibilterra", "Gibuti", "Giordania", "Grecia", "Grenada", "Groenlandia", "Guadalupa", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea-Bissau", "Guinea Equatoriale", "Guyana", "Guyana francese", "Haiti", "Honduras", "Hong Kong", "India", "Indonesia", "Iran", "Iraq", "Irlanda", "Islanda", "Isola Bouvet", "Isola di Man", "Isola di Natale", "Isola Norfolk", "Isole ", "Isole BES", "Isole Cayman", "Isole Cocos (Keeling)", "Isole Cook", "F", "Isole Falkland", "Isole Heard e McDonald", "Isole Marianne Settentrionali", "Isole Marshall", "Isole minori esterne degli Stati Uniti", "Isole Pitcairn", "Isole Salomone", "Isole Vergini britanniche", "Isole Vergini americane", "Israele", "Italia", "Jersey", "Kazakistan", "Kenya", "Kirghizistan", "Kiribati", "Kuwait", "Laos", "Lesotho", "Lettonia", "Libano", "Liberia", "Libia", "Liechtenstein", "Lituania", "Lussemburgo", "Macao", "Macedonia", "Madagascar", "Malawi", "Malesia", "Maldive", "Mali", "Malta", "Marocco", "Martinica", "Mauritania", "Mauritius", "Mayotte", "Messico", "Micronesia", "Moldavia", "Mongolia", "Montenegro", "Montserrat", "Mozambico", "Namibia", "Nauru", "Nepal", "Nicaragua", "Niger", "Nigeria", "Niue", "Norvegia", "Nuova Caledonia", "Nuova Zelanda", "Oman", "Paesi Bassi", "Pakistan", "Palau", "Palestina", "Panam", "Papua Nuova Guinea", "Paraguay", "Per", "Polinesia Francese", "Polonia", "Porto Rico", "Portogallo", "Monaco", "Qatar", "Regno Unito", "RD del Congo", "Rep. Ceca", "Rep. Centrafricana", "Rep. del Congo", "Rep. Dominicana", "Riunione", "Romania", "Ruanda", "Russia", "Sahara Occidentale", "Saint Kitts e Nevis", "Santa Lucia", "Sant'Elena, Ascensione e Tristan da Cunha", "Saint Vincent e Grenadine", "Saint-Barth", "Saint-Martin", "Saint-Pierre e Miquelon", "Samoa", "Samoa Americane", "San Marino", "S", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Sint Maarten", "Siria", "Slovacchia", "Slovenia", "Somalia", "Spagna", "Sri Lanka", "Stati Uniti", "Sudafrica", "Sudan", "Sudan del Sud", "Suriname", "Svalbard e Jan Mayen", "Svezia", "Svizzera", "Swaziland", "Taiwan", "Tagikistan", "Tanzania", "Terre australi e antartiche francesi", "Territorio britannico dell'oceano Indiano", "Thailandia", "Timor Est", "Togo", "Tokelau", "Tonga", "Trinidad e Tobago", "Tunisia", "Turchia", "Turkmenistan", "Turks e Caicos", "Tuvalu", "Ucraina", "Uganda", "Ungheria", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Wallis e Futuna", "Yemen", "Zambia", "Zimbabwe", " " }));
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Svizzera", "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antartide", "Antigua e Barbuda", "Arabia Saudita", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaigian", "Bahamas", "Bahrein", "Bangladesh", "Barbados", "Belgio", "Belize", "Benin", "Bermuda", "Bhutan", "Bielorussia", "Birmania", "Bolivia", "Bosnia ed Erzegovina", "Botswana", "Brasile", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambogia", "Camerun", "Canada", "Capo Verde", "Ciad", "Cile", "Cina", "Cipro", "Citt", "Colombia", "Comore", "Corea del Nord", "Corea del Sud", "Costa d'Avorio", "Costa Rica", "Croazia", "Cuba", "Cura", "Danimarca", "Dominica", "Ecuador", "Egitto", "El Salvador", "Emirati Arabi Uniti", "Eritrea", "Estonia", "Etiopia", "Figi", "Filippine", "Finlandia", "Francia", "Gabon", "Gambia", "Georgia", "Georgia del Sud e isole Sandwich meridionali", "Germania", "Ghana", "Giamaica", "Giappone", "Gibilterra", "Gibuti", "Giordania", "Grecia", "Grenada", "Groenlandia", "Guadalupa", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea-Bissau", "Guinea Equatoriale", "Guyana", "Guyana francese", "Haiti", "Honduras", "Hong Kong", "India", "Indonesia", "Iran", "Iraq", "Irlanda", "Islanda", "Isola Bouvet", "Isola di Man", "Isola di Natale", "Isola Norfolk", "Isole ", "Isole BES", "Isole Cayman", "Isole Cocos (Keeling)", "Isole Cook", "F", "Isole Falkland", "Isole Heard e McDonald", "Isole Marianne Settentrionali", "Isole Marshall", "Isole minori esterne degli Stati Uniti", "Isole Pitcairn", "Isole Salomone", "Isole Vergini britanniche", "Isole Vergini americane", "Israele", "Italia", "Jersey", "Kazakistan", "Kenya", "Kirghizistan", "Kiribati", "Kuwait", "Laos", "Lesotho", "Lettonia", "Libano", "Liberia", "Libia", "Liechtenstein", "Lituania", "Lussemburgo", "Macao", "Macedonia", "Madagascar", "Malawi", "Malesia", "Maldive", "Mali", "Malta", "Marocco", "Martinica", "Mauritania", "Mauritius", "Mayotte", "Messico", "Micronesia", "Moldavia", "Mongolia", "Montenegro", "Montserrat", "Mozambico", "Namibia", "Nauru", "Nepal", "Nicaragua", "Niger", "Nigeria", "Niue", "Norvegia", "Nuova Caledonia", "Nuova Zelanda", "Oman", "Paesi Bassi", "Pakistan", "Palau", "Palestina", "Panam", "Papua Nuova Guinea", "Paraguay", "Per", "Polinesia Francese", "Polonia", "Porto Rico", "Portogallo", "Monaco", "Qatar", "Regno Unito", "RD del Congo", "Rep. Ceca", "Rep. Centrafricana", "Rep. del Congo", "Rep. Dominicana", "Riunione", "Romania", "Ruanda", "Russia", "Sahara Occidentale", "Saint Kitts e Nevis", "Santa Lucia", "Sant'Elena, Ascensione e Tristan da Cunha", "Saint Vincent e Grenadine", "Saint-Barth", "Saint-Martin", "Saint-Pierre e Miquelon", "Samoa", "Samoa Americane", "San Marino", "S", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Sint Maarten", "Siria", "Slovacchia", "Slovenia", "Somalia", "Spagna", "Sri Lanka", "Stati Uniti", "Sudafrica", "Sudan", "Sudan del Sud", "Suriname", "Svalbard e Jan Mayen", "Svezia", "Svizzera", "Swaziland", "Taiwan", "Tagikistan", "Tanzania", "Terre australi e antartiche francesi", "Territorio britannico dell'oceano Indiano", "Thailandia", "Timor Est", "Togo", "Tokelau", "Tonga", "Trinidad e Tobago", "Tunisia", "Turchia", "Turkmenistan", "Turks e Caicos", "Tuvalu", "Ucraina", "Uganda", "Ungheria", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Wallis e Futuna", "Yemen", "Zambia", "Zimbabwe", " " }));
         naz.add(jComboBox1, new java.awt.GridBagConstraints());
 
         posizione.add(naz);
@@ -86,10 +100,14 @@ public class AddArnia extends javax.swing.JDialog {
         });
         getContentPane().add(invio, java.awt.BorderLayout.SOUTH);
 
+        abitata.setText("abitata");
+        getContentPane().add(abitata, java.awt.BorderLayout.LINE_END);
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void invioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_invioActionPerformed
+        setValori();
         raiseChange();
         this.setVisible(false);
     }//GEN-LAST:event_invioActionPerformed
@@ -140,6 +158,7 @@ public class AddArnia extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox abitata;
     private javax.swing.JButton invio;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JScrollPane jScrollPane1;
