4d461ee1059a33a9007c2fce7969024ce81c2223
AlessandroAloise <54986411+AlessandroAloise@users.noreply.github.com>
2020-11-19 15:31:18 +0100

    modifica del form
    modifica del form addRegina ho aggiunto la possibilità di dire in che arina si vuole aggiungere l'ape regina e colegato al db

--- APIARIO/src/AddRegina.java
+++ APIARIO/src/AddRegina.java
@@ -3,12 +3,13 @@ import java.awt.Frame;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-
 /**
  *
  * @author alessandro.aloise
  */
 public class AddRegina extends javax.swing.JDialog {
+
+    public String[] valori = new String[2];
 
     /**
      * Creates new form AddRegina
@@ -18,9 +19,8 @@ public class AddRegina extends javax.swing.JDialog {
         initComponents();
     }
 
-    
     private void raiseChange() {
-        PropertyChangeEvent event = new PropertyChangeEvent(this, "regina", false,datePicker1.getDate());
+        PropertyChangeEvent event = new PropertyChangeEvent(this, "regina", false, datePicker.getDate());
         PropertyChangeListener[] listeners = this.getPropertyChangeListeners();
         synchronized (listeners) {
             for (PropertyChangeListener e : listeners) {
@@ -28,23 +28,41 @@ public class AddRegina extends javax.swing.JDialog {
             }
         }
     }
-    
-    
+
+    public void setValori() {
+        valori[0] = NarinaU.getText();
+        valori[1] = datePicker.getText();
+
+    }
+
+    public String getNomeArina() {
+        setValori();
+        return valori[0];
+    }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        data = new javax.swing.JLabel();
-        datePicker1 = new com.github.lgooddatepicker.components.DatePicker();
+        jPanel1 = new javax.swing.JPanel();
+        NomeArina = new javax.swing.JLabel();
+        NarinaU = new javax.swing.JTextField();
         invio = new javax.swing.JButton();
+        dataPanel = new javax.swing.JPanel();
+        data = new javax.swing.JLabel();
+        datePicker = new com.github.lgooddatepicker.components.DatePicker();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Data Regina");
         setMinimumSize(new java.awt.Dimension(300, 60));
 
-        data.setText("Data:");
-        getContentPane().add(data, java.awt.BorderLayout.LINE_START);
-        getContentPane().add(datePicker1, java.awt.BorderLayout.CENTER);
+        jPanel1.setLayout(new java.awt.GridLayout(1, 0));
+
+        NomeArina.setText("Nome arina");
+        jPanel1.add(NomeArina);
+        jPanel1.add(NarinaU);
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
 
         invio.setText("Invio");
         invio.addActionListener(new java.awt.event.ActionListener() {
@@ -53,6 +71,14 @@ public class AddRegina extends javax.swing.JDialog {
             }
         });
         getContentPane().add(invio, java.awt.BorderLayout.PAGE_END);
+
+        dataPanel.setLayout(new java.awt.BorderLayout());
+
+        data.setText("Data:");
+        dataPanel.add(data, java.awt.BorderLayout.LINE_START);
+        dataPanel.add(datePicker, java.awt.BorderLayout.CENTER);
+
+        getContentPane().add(dataPanel, java.awt.BorderLayout.PAGE_START);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -105,8 +131,12 @@ public class AddRegina extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField NarinaU;
+    private javax.swing.JLabel NomeArina;
     private javax.swing.JLabel data;
-    private com.github.lgooddatepicker.components.DatePicker datePicker1;
+    private javax.swing.JPanel dataPanel;
+    private com.github.lgooddatepicker.components.DatePicker datePicker;
     private javax.swing.JButton invio;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
